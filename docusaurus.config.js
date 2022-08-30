cad419d2588ca1eb1cd5b478210fa551e54983ee
mamariomiamo <mazt1025@hotmail.com>
2022-08-30 11:29:14 +0800

    Add fingerprint.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -155,7 +155,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} NUS Drone Club. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
